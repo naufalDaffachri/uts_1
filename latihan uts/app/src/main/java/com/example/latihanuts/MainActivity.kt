b206001e841e18a43d48b9c03f005581c67f1868
naufaldaffachri1745 <muhammadnaufaldaffachri@mail.ugm.ac.id>
2024-10-07 11:39:38 +0700

    uts no 1

---
+++ latihan uts/app/src/main/java/com/example/latihanuts/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.latihanuts
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.latihanuts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
